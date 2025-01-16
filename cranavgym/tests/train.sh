32cecddb11dceea266b4f736dd1aca22cc6fc505
Paris Chatz <parischatz94@gmail.com>
2025-01-16 10:27:26 +0000

    Addition of drone env training example

--- cranavgym/tests/train.sh
+++ cranavgym/tests/train.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
+# Wheeled robot training
 source ~/cranfield-navigation-gym/cranavgym/tests/setup_env.sh
 python3 train.py -run-name test1 -algo TD3 -lr 0.03 -max-training-steps 256
 
-source ~/cranfield-navigation-gym/cranavgym/tests/setup_env.sh
-python3 train.py -run-name test2 -algo PPO -lr 0.003 -max-training-steps 256
+# Drone training
+python3 train_drone_simple.py -run-name gdp-test -gym-env-id DroneNavigationGDP-v0 -max-training-steps 150 -lr 0.0003 -obs "camera" -algo TD3
